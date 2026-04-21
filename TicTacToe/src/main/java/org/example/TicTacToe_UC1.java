05ad9eae9d906a94c792256eee9f7035162b2de5
gaalidhanush2008-boop <gaali.dhanush2008@gmail.com>
2026-04-21 10:23:44 +0530

    Adding UC1 code

---
+++ TicTacToe/src/main/java/org/example/TicTacToe_UC1.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class TicTacToe {
+
+    static char[][] board = new char[3][3];
+    Run|Debug
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+    }
+    static void initializeBoard() {-
+    }
+    static void printBoard() {
+        system.out.printlln|x; "--------------------"};
+        for [int row = 0; row  < 3; row++] {-
+    }
+}
